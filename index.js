4d46ea83e926c739a3c41886e5fb5c7645e4ee5e
Sarah Veness <sarahhveness@gmail.com>
2016-09-14 01:30:47 +0000

    all error messages logged

--- index.js
+++ index.js
@@ -8,16 +8,35 @@ var fs = require('fs');
 
 var api_token = process.env['GITHUB_API_TOKEN'];
 
-var inputs = process.argv.slice(2);
-var owner = inputs[0];
-var repo = inputs[1];
-
+var owner = process.argv[2];
+var repo = process.argv[3];
 var apiRoot = "https://api.github.com";
 
+if (api_token === undefined) {
+  console.log("Please check that you have a .env file");
+  process.exit(1);
+} else if (process.argv.length !== 4) {
+  console.log("Please pass only two arguments, first: repo owner, second: repo name");
+  process.exit(1);
+}
 
-getRepoContributors(owner, repo, api_token, function(err, contributors) {
-  contributors.forEach(function(value, index) {
-    var filename = "./avatars/"+value.login+".jpg";
-    downloadImageByURL(value.avatar_url, filename);
-  })
+getRepoContributors(owner, repo, api_token, function(err, incomingMessage, contributors) {
+  if (err) {
+    console.log(err);
+  } else if (incomingMessage.caseless.dict.status !== '200 OK') {
+    console.log("Your credentials are incorrect");
+  } else if (!Array.isArray(contributors)) {
+    console.log(owner, 'is not a valid owner, or', repo, 'is not a valid repo. Check your input and try again');
+  } else {
+    contributors.forEach(function(value, index) {
+      var directory = "./avatars";
+      if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory);
+        console.log(`Directory ${directory} has been created`);
+      }
+        var filename = directory + '/' + value.login + ".jpg";
+      downloadImageByURL(value.avatar_url, filename);
+    });
+  }
 });
+
